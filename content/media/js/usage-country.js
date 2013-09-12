a4ef95db1c737014b3c27920b0b75db09f525a12
Matthew Harris <mattbenharris@gmail.com>
2013-09-12 11:04:59 -0700

    MBH: labels

--- content/media/js/usage-country.js
+++ content/media/js/usage-country.js
@@ -12,7 +12,7 @@ var color = d3.scale.ordinal()
 .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
 var arc = d3.svg.arc()
-.outerRadius(radius - 10)
+.outerRadius(radius - 50)
 .innerRadius(0);
 
 var pie = d3.layout.pie()
