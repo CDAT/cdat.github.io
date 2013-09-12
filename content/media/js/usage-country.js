98627419cb940397fbaab6a08ebe1b858b42211a
Matthew Harris <mattbenharris@gmail.com>
2013-09-12 13:22:01 -0700

    MBH: fixing total count div placement

--- content/media/js/usage-country.js
+++ content/media/js/usage-country.js
@@ -76,7 +76,7 @@ d3.json(json_url, function(error, data) {
       .attr("y", function(d) { return y(d[1]); })
       .attr("height", function(d) { return height - y(d[1]); });
 
+  $('#count').append("<p><b>The total number of unique IPs is <i>" + totalCount + "</i></b></p>");
 });
 
-$('#count').append("<p> The total number of unique IPs is " + totalCount + "</p>");
 
