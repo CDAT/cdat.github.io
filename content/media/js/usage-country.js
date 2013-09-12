2ad18bedcd06c5b1a5f0506de319d8edbaa3b519
Matthew Harris <mattbenharris@gmail.com>
2013-09-12 10:52:39 -0700

    MBH: label hell

--- content/media/js/usage-country.js
+++ content/media/js/usage-country.js
@@ -4,26 +4,26 @@ var json_url = "http://uvcdat.llnl.gov/UVCDATUsage/log/json/country/?days=0";
 
 // Set dimensions of the chart
 var width = 960,
-    height = 500,
-    radius = Math.min(width, height) / 2;
+height = 500,
+radius = Math.min(width, height) / 2;
 
 // Set colors to use for each slice
 var color = d3.scale.ordinal()
-    .range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
+.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
 var arc = d3.svg.arc()
-    .outerRadius(radius - 10)
-    .innerRadius(0);
+.outerRadius(radius - 10)
+.innerRadius(0);
 
 var pie = d3.layout.pie()
-    .sort(null)
-    .value(function(d) { return d[1]; });
+.sort(null)
+.value(function(d) { return d[1]; });
 
 var svg = d3.select("#top").append("svg")
-    .attr("width", width)
-    .attr("height", height)
-    .append("g")
-    .attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
+.attr("width", width)
+.attr("height", height)
+.append("g")
+.attr("transform", "translate(" + width / 2 + "," + height / 2 + ")");
 
 // Get JSON (must be a URL on this server -- see "Same origin policy" on Wikipedia)
 d3.json(json_url, function(error, data) {
@@ -41,27 +41,32 @@ d3.json(json_url, function(error, data) {
   /* Draw stuff */
 
   var g = svg.selectAll(".arc")
-      .data(pie(data))
-      .enter().append("g")
-      .attr("class", "arc");
+  .data(pie(data))
+  .enter().append("g")
+  .attr("class", "arc");
 
   g.append("path")
-      .attr("d", arc)
-      .style("fill", function(d) { return color(d.data[0]); });
+  .attr("d", arc)
+  .style("fill", function(d) { return color(d.data[0]); });
 
   g.append("text")
-      .attr("transform", function(d) { return "translate(" + arc.centroid(d) + ")"; })
-      //.attr("dy", ".35em")
-      .attr("dy", function(d){
-        var c = arc.centroid(d);
-        var x = c[0];
-        var y = c[1];
-        var h = Math.sqrt(x*x + y*y);
-        var dy = y/h * obj.labelRadius; 
-        dy=dy*fontSizeParam*(.14/heightParam);
-        return (dy)+"em";
-      })
-      .style("text-anchor", "middle")
-      .text(function(d) { return d.data[0]; });
+  .attr("transform", function(d) {
+    var c = arc.centroid(d),
+    x = c[0],
+    y = c[1],
+    // pythagorean theorem for hypotenuse
+    h = Math.sqrt(x*x + y*y);
+    return "translate(" + (x/h * labelr) +  ',' +
+      (y/h * labelr) +  ")"; 
+  })
+  .attr("text-anchor", function(d) {
+    // are we past the center?
+    return (d.endAngle + d.startAngle)/2 > Math.PI ?
+      "end" : "start";
+  })
+  .attr("dy", ".35em")
+  .text(function(d) { return d.data[0]; });
+
+
 
 });
