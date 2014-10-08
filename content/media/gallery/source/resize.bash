3c30217ef4cf8704ba0fc57d7476d42ec34e327e
Matthew Harris <mattbenharris@gmail.com>
2014-10-08 10:03:38 -0700

    MBH: bash script not working

--- content/media/gallery/source/resize.bash
+++ content/media/gallery/source/resize.bash
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-for f in ../fullsize/*.png; do convert $f -resize 190x190 ../thumbnails/$f; done
+#for f in ../fullsize/*.png; do convert $f -resize 190x190 ../thumbnails/$f; done
+
+
+
+#for f in *; do convert $f -resize 190x190 ../thumbnails/$f; done
