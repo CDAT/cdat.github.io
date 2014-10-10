81359f1f47d8f451de4020540b3a513d9a1b2f7b
Thomas Maxwell <thomas.maxwell@nasa.gov>
2014-10-10 18:57:31 -0400

    Add cubed sphere plot

---
+++ content/media/gallery/source/vcs3D_cubed_sphere_volume.py
@@ -0,0 +1,16 @@
+import vcs, cdms2, sys
+
+x = vcs.init()
+f = cdms2.open( "f1850c5_t2_ANN_climo-native.nc" )   
+v = f["RELHUM"] 
+dv3d = vcs.get3d_scalar()
+dv3d.ScaleTransferFunction = [ 78.7, 132.0 ]
+dv3d.ScaleColormap = [ 75.4, 100.0 ]
+dv3d.ScaleOpacity = [0.27, 1.0]
+dv3d.PointSize = [5, 2]
+dv3d.VerticalScaling = [ 1.7 ]
+dv3d.ToggleVolumePlot = vcs.on
+dv3d.ToggleSphericalProj = vcs.on
+dv3d.Camera = {'Position': (-108.11442471080369, -476.65927617219285, 84.45227482307195), 'ViewUp': ( 0.0, 0.0, 1.0), 'FocalPoint': (0, 0, 0)}
+x.plot( v, dv3d, grid_file="ne120np4_latlon.nc" )
+x.interact()
