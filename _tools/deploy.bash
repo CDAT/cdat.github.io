e234df17c10d9d347fedfd06b90db7d7eb0e4746
Matthew Harris <mattbenharris@gmail.com>
2016-09-07 08:44:21 -0700

    MBH: adding a deploy script

---
+++ _tools/deploy.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if [ $# -eq 0 ]
+then
+	echo "Usage: deploy.bash /path/to/site-home/"
+	exit 1;
+fi
+
+base_dir=$1;
+media_dir=$base_dir'_site';
+
+cd $base_dir;
+git pull;
+jekyll b;
+mkdir _site/gallery/thumbnails;
+./_tools/resize.bash $media_dir;
+gs
